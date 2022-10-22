ac354a0b8604474d6abce194479950f78fbddccb
atharva-gadekar <atharvagadekar@gmail.com>
2022-10-23 04:28:26 +0530

    Deleted Unnecessary files

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
 import "../styles/globals.css";
-import "../styles/Category_Card.css";
-import "../styles/Category.css";
 
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/router";
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
+	const router = useRouter();
+	return (
+		<NextUIProvider>
+			<Component {...pageProps} />
+		</NextUIProvider>
+	);
 }
 
 export default MyApp;
