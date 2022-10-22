216245c8bfa37d3c8b314ad9ef3ac67bfb64806a
guptamanas149@gmail.com <guptamanas149@gmail.com>
2022-10-22 23:41:53 +0530

    added header

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import styles from "../styles/Home.module.css";
 import ListItem from "../components/interfaces/ListItem";
 import Footer from "../components/interfaces/Footer";
 import Category from "./../components/Sections/Category";
-
+import FoundHeader from "./../components/interfaces/FoundHeader";
 export default function Home() {
-	return (
-		<>
-			<Category />
-			<ListItem />
-			<Footer />
-		</>
-	);
+    return (
+        <>
+            <FoundHeader />
+            <Category />
+            <ListItem />
+            <Footer />
+        </>
+    );
 }
