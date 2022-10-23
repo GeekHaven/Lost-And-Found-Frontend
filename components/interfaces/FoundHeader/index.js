a16916885d86cc1f2d7d2847315232992800b60f
atharva-gadekar <atharvagadekar@gmail.com>
2022-10-24 02:09:37 +0530

    Made Found Header Responsive

--- components/interfaces/FoundHeader/index.js
+++ components/interfaces/FoundHeader/index.js
@@ -5,12 +5,9 @@ export default function FoundHeader() {
 		<>
 			<div className="flex flex-col gap-y-14 ml-4">
 				<h1 className="font-bold text-3xl">Found Items</h1>
-				<div className="flex flex-row gap-4 flex-wrap -mt-5">
-					<div className="flex flex-row h-14 ">
-						<div
-							className="rounded-l-md bg-[#555555] border-solid border-2 border-black"
-							style={{ width: "40vw" }}
-						>
+				<div className="flex flex-row gap-4 flex-wrap -mt-5 ">
+					<div className="flex flex-row h-14 sm:w-full">
+						<div className="rounded-l-md bg-[#555555] border-solid border-2 border-black w-[40vw] sm:w-full">
 							<input
 								type="text"
 								name="text"
@@ -20,10 +17,10 @@ export default function FoundHeader() {
 							/>
 						</div>
 						<div className="bg-[#000000] px-3 py-2 rounded-r-md flex items-center ">
-							<Image src={search} width={30} height={30} />
+							<Image src={search} width={30} height={30} className="scale-50" />
 						</div>
 					</div>
-					<button className=" py-0 px-14  h-14 rounded-md  border-solid border-2  border-[#121212] bg-white text-[#121212] font-normal text-xl">
+					<button className=" py-0 px-14  h-14 rounded-md  border-solid border-2  border-[#121212] bg-white text-[#121212] font-normal text-xl sm:w-[40%] sm:text-lg sm:p-0">
 						Add found item
 					</button>
 				</div>
