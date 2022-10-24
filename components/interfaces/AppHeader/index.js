e4541e125ced4dc9e557f7f5b15e37b45e6190bc
Yash Gupta <gyash6696@gmail.com>
2022-10-24 14:51:58 +0530

    Instruction page done(Not much responsive yet)

--- components/interfaces/AppHeader/index.js
+++ components/interfaces/AppHeader/index.js
@@ -1,20 +1,22 @@
 export default function AppHeader() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center  bg-[#df5d4b] gap-y-14 relative">
-                <h1 className="font-semibold text-6xl text-center mt-20 text-[#ffffff]">
+            <div className="flex flex-col items-center justify-center  bg-[#df5d4b] gap-y-20 relative">
+                <h1 className="text-6xl font-medium text-center mt-20 text-[#ffffff]"
+                style={{letterSpacing: "2px"}}
+                >
                     What have you Lost/Found <br /> today?
                 </h1>
                 <div className="flex flex-row" style={{ gap: "10px" }}>
                     <button
                         className="bg-[#385a64] py-4 text-[#ffffff] rounded-lg text-xl font-normal"
-                        style={{ paddingLeft: "5rem", paddingRight: "5rem" }}
+                        style={{ display:"block" ,position:"absolute",left:"30%",transform:"translateY(-50%) translateX(-30%)", width:"25vw" }}
                     >
                         Lost Item
                     </button>
                     <button
                         className="bg-[#385a64] py-4 text-[#ffffff] rounded-lg text-xl font-normal"
-                        style={{ paddingLeft: "5rem", paddingRight: "5rem" }}
+                        style={{ display:"block" ,position:"absolute",left:"35%",transform:"translateY(-50%) translateX(70%)", width:"25vw"  }}
                     >
                         Found Item
                     </button>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Found from "./found";
 import Lost from "./lost";
+import Instructions from "./instructions";
 
 export default function Home() {
 	return (
