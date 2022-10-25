53e26a2acfc4d50900b24b5408a2b0ed93d1d989
guptamanas149@gmail.com <guptamanas149@gmail.com>
2022-10-25 20:13:14 +0530

    added hiw and end section

--- components/Sections/End.js
+++ components/Sections/End.js
@@ -1,3 +1,14 @@
 export default function End() {
-	return <div>End</div>;
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <div className="text-5xl font-normal text-center">
+                Ready to meet up with <br /> your long lost things?
+            </div>
+            <div className="mt-[5rem]">
+                <button className="p-3 rounded-md	 pl-8 pr-8 bg-[#5B5B5B] text-black">
+                    Get Started
+                </button>
+            </div>
+        </div>
+    );
 }
--- components/Sections/HIW.js
+++ components/Sections/HIW.js
@@ -1,3 +1,36 @@
 export default function HIW() {
-	return <div>HIW</div>;
+    return (
+        <div className="flex flex-col items-center justify-center gap-y-10">
+            <div className="flex flex-col items-center justify-center gap-y-2">
+                <div className="text-5xl font-semibold">How it works!</div>
+                <div className="font-normal text-2xl">in 3 simple steps</div>
+            </div>
+            <div className="flex flex-row gap-x-28">
+                <div className="flex flex-col gap-y-6 items-center">
+                    <div className="bg-[#d9d9d9] w-60  h-72  "></div>
+                    <div className="font-normal text-base text-center">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Mi tincidunt pharetra Lorem ipsum dolor sit amet,
+                        consectetu
+                    </div>
+                </div>
+                <div className="flex flex-col gap-y-6 items-center">
+                    <div className="bg-[#d9d9d9] w-60  h-72"></div>
+                    <div className="font-normal text-base text-center">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Mi tincidunt pharetra Lorem ipsum dolor sit amet,
+                        consectetu
+                    </div>
+                </div>
+                <div className="flex flex-col gap-y-6 items-center">
+                    <div className="bg-[#d9d9d9] w-60  h-72"></div>
+                    <div className="font-normal text-base text-center">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Mi tincidunt pharetra Lorem ipsum dolor sit amet,
+                        consectetu
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
