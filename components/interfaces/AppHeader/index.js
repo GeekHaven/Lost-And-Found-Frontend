082eded86ec771049043a18ebebcbeb5a655ec5c
guptamanas149@gmail.com <guptamanas149@gmail.com>
2022-10-29 16:29:58 +0530

    changed instructions page

--- components/interfaces/AppHeader/index.js
+++ components/interfaces/AppHeader/index.js
@@ -1,22 +1,23 @@
 export default function AppHeader() {
     return (
         <>
-            <div className="flex flex-col items-center justify-center  bg-[#df5d4b] gap-y-20 relative">
-                <h1 className="text-6xl font-medium text-center mt-20 text-[#ffffff]"
-                style={{letterSpacing: "2px"}}
-                >
-                    What have you Lost/Found <br /> today?
+            <div
+                className="flex flex-col gap-y-16 pt-16 pb-32 items-center justify-center"
+                style={{ boxShadow: "0px 4px 16px rgba(0,0,0,0.15)" }}
+            >
+                <h1 className="font-semibold text-7xl text-[#575C75]">
+                    What have you Lost/Found today ?
                 </h1>
-                <div className="flex flex-row" style={{ gap: "10px" }}>
+                <div className="flex flex-row gap-14 flex-wrap ">
                     <button
-                        className="bg-[#385a64] py-4 text-[#ffffff] rounded-lg text-xl font-normal"
-                        style={{ display:"block" ,position:"absolute",left:"30%",transform:"translateY(-50%) translateX(-30%)", width:"25vw" }}
+                        className="py-3.5 px-24   rounded-lg   bg-[#304AC1] text-[#ffffff] font-semibold text-2xl  sm:text-lg"
+                        style={{ boxShadow: "0px 8px 20px -10px #10237b" }}
                     >
                         Lost Item
                     </button>
                     <button
-                        className="bg-[#385a64] py-4 text-[#ffffff] rounded-lg text-xl font-normal"
-                        style={{ display:"block" ,position:"absolute",left:"35%",transform:"translateY(-50%) translateX(70%)", width:"25vw"  }}
+                        className="py-3.5 px-24   rounded-lg   bg-[#304AC1] text-[#ffffff] font-semibold text-2xl  sm:text-lg"
+                        style={{ boxShadow: "0px 8px 20px -10px #10237b" }}
                     >
                         Found Item
                     </button>
