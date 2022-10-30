2018d6b3f5f37c57ff3db975f1adea604b3cad4d
atharva-gadekar <atharvagadekar@gmail.com>
2022-10-31 01:29:10 +0530

    Added Navbar. Yet to add hamburger menu

--- components/interfaces/Navbar/index.js
+++ components/interfaces/Navbar/index.js
@@ -1,38 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
 import { BiUserCircle } from "react-icons/bi";
+import logo from "../../../assets/logo/nav_logo.svg";
+
 const navLinks = [
-    {
-        title: "Home",
-        href: "/home",
-    },
-    {
-        title: "Instructions",
-        href: "/instructions",
-    },
-    {
-        title: "Lost",
-        href: "/lost",
-    },
-    {
-        title: "Found",
-        href: "/found",
-    },
+	{
+		title: "Home",
+		href: "/home",
+	},
+	{
+		title: "Instructions",
+		href: "/instructions",
+	},
+	{
+		title: "Lost",
+		href: "/lost",
+	},
+	{
+		title: "Found",
+		href: "/found",
+	},
 ];
+
 export default function Navbar() {
-    return (
-        <div
-            className="w-full h-14 flex flex-row items-center justify-end gap-x-14 p-8 pr-6"
-            style={{ backgroundColor: "#ffffff" }}
-        >
-            {navLinks.map((navLink, i) => (
-                <Link href={navLink.href} key={i}>
-                    <a className="text-dark-300 text-lg font-normal">
-                        {navLink.title}
-                    </a>
-                </Link>
-            ))}
-            <div></div>
-        </div>
-    );
+	return (
+		<div
+			className="w-full h-14 flex flex-row items-center justify-between sm:justify-center gap-x-14 p-12"
+			style={{ backgroundColor: "#ffffff" }}
+		>
+			<div className=" ml-16 text-[#304AC1] flex flex-row items-center justify-center w-96">
+				<Image src={logo} />
+				<div className="ml-4 font-semibold text-2xl w-64">Lost&Found IIITA</div>
+			</div>
+			<div className="ml-[20vw] lg:ml-0 flex flex-row items-center justify-evenly w-[50vw] sm:hidden">
+				{navLinks.map((navLink, i) => (
+					<div className="text-dark-300 text-lg font-normal hover:text-[#304AC1] hover:border-b-2 px-2 hover:border-[#304AC1] ml-2 mr-2">
+						<Link href={navLink.href} key={i}>
+							{navLink.title}
+						</Link>
+					</div>
+				))}
+			</div>
+
+			<div></div>
+		</div>
+	);
 }
