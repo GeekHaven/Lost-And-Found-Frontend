12e0879871d7db176f4accb1cf6348d1a9f51e88
atharva-gadekar <atharvagadekar@gmail.com>
2022-11-08 03:08:01 +0530

    More changes to Forms

--- pages/add/found/index.js
+++ pages/add/found/index.js
@@ -1,8 +1,54 @@
 import img1 from "../../../assets/Upload.svg";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 import Footer from "../../../components/interfaces/Footer/index";
+import { FileUploader } from "react-drag-drop-files";
+const fileTypes = ["JPG", "PNG", "GIF"];
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 export default function found_form() {
+	const [file, setFile] = useState(null);
+	const [fileDataURL, setFileDataURL] = useState(null);
+	const handleChange = (file) => {
+		setFile(file);
+	};
+	function remove() {
+		setFile(null);
+	}
+
+	async function submit() {
+		let data = new FormData();
+		data["image"] = file;
+		let res = await post("/uploadimage", data);
+		if (res) {
+			let url = res.data?.url;
+			if (url) {
+				let body = {};
+			}
+		}
+	}
+
+	useEffect(() => {
+		let fileReader,
+			isCancel = false;
+		if (file) {
+			fileReader = new FileReader();
+			fileReader.onload = (e) => {
+				const { result } = e.target;
+				if (result && !isCancel) {
+					setFileDataURL(result);
+				}
+			};
+			fileReader.readAsDataURL(file);
+		}
+		return () => {
+			isCancel = true;
+			if (fileReader && fileReader.readyState === 1) {
+				fileReader.abort();
+			}
+		};
+	}, [file]);
+
 	return (
 		<>
 			<div className="h-screen flex">
@@ -10,17 +56,69 @@ export default function found_form() {
 					<h1 className="font-semibold text-5xl mb-10 text-[#304AC1]">
 						Add Found Item
 					</h1>
-					<div className="text-sm box-border h-24 flex items-center justify-center flex-wrap flex-col w-96 border-2 border-solid border-slate-400 rounded-md mb-5 bg-[#EBEFFA] pl-3 pr-3">
-						<div className="text-center w-full text-base opacity-40">
-							Add Image
+					{file === null ? (
+						<div
+							className="text-sm box-border flex items-center justify-center flex-wrap flex-col w-96 border-2 border-solid border-slate-400 rounded-md mb-5 bg-[#EBEFFA] pl-3 pr-3"
+							style={{ position: "relative" }}
+						>
+							<div>
+								<div className="text-center w-full text-base opacity-40">
+									Add Image
+								</div>
+								<div className=" text-center w-full scale-75 opacity-40">
+									<Image src={img1} className="" />
+								</div>
+
+								<div className="text-center w-full text-xs opacity-40 ">
+									Drag or drop or browse a file
+								</div>
+							</div>
+							<div
+								style={{
+									opacity: 0,
+									position: "absolute",
+								}}
+							>
+								<FileUploader
+									handleChange={handleChange}
+									name="file"
+									types={fileTypes}
+								/>
+							</div>
 						</div>
-						<div className=" text-center w-full scale-75 opacity-40">
-							<Image src={img1} className="" />
+					) : (
+						<div
+							className="text-sm box-border flex py-4 pt-6 items-center justify-center flex-wrap flex-col w-96 border-2 border-solid border-slate-400 rounded-md mb-5 bg-[#EBEFFA] pl-3 pr-3"
+							style={{ position: "relative" }}
+						>
+							<div className="" style={{ width: "50%", position: "relative" }}>
+								<img src={fileDataURL} alt="preview" />
+								<div
+									style={{
+										position: "absolute",
+										top: "-15px",
+										right: "-15px",
+									}}
+									onClick={remove}
+								>
+									<IoCloseCircleOutline />
+								</div>
+							</div>
 						</div>
-						<div className="text-center w-full text-xs opacity-40">
-							Drag or drop or browse a file
-						</div>
+					)}
+					<div
+						style={{
+							opacity: 0,
+							position: "absolute",
+						}}
+					>
+						<FileUploader
+							handleChange={handleChange}
+							name="file"
+							types={fileTypes}
+						/>
 					</div>
+
 					<div className="text-sm box-border h-11 w-96 border-2 border-solid border-slate-400 rounded-md mb-5 flex align-middle bg-[#EBEFFA] pl-3">
 						<input type="text" placeholder="Title" className="bg-transparent" />
 					</div>
@@ -47,6 +145,7 @@ export default function found_form() {
 					<button
 						className="bg-[#304AC1] text-[#EBEFFA] h-11 w-96 rounded-md select-none"
 						type="button"
+						onClick={submit}
 					>
 						Add Found Item
 					</button>
--- pages/add/lost/index.js
+++ pages/add/lost/index.js
@@ -1,6 +1,7 @@
 import img1 from "../../../assets/Upload.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
+// import fs from "fs";
 import Footer from "../../../components/interfaces/Footer/index";
 import { FileUploader } from "react-drag-drop-files";
 import img2 from "../../../assets/illustrations/homepage2.png";
@@ -10,9 +11,48 @@ const fileTypes = ["JPG", "PNG", "GIF"];
 
 export default function lost_form() {
 	const [file, setFile] = useState(null);
+	const [fileDataURL, setFileDataURL] = useState(null);
 	const handleChange = (file) => {
 		setFile(file);
 	};
+	function remove() {
+		setFile(null);
+	}
+
+	async function submit(){
+		let data = new FormData();
+		data["image"] = file;
+		let res = await post("/uploadimage", data);
+		if (res) {
+			let url = res.data?.url;
+			if(url){
+				let body = {
+					
+				}
+			}
+		}	
+	}
+
+	useEffect(() => {
+		let fileReader,
+			isCancel = false;
+		if (file) {
+			fileReader = new FileReader();
+			fileReader.onload = (e) => {
+				const { result } = e.target;
+				if (result && !isCancel) {
+					setFileDataURL(result);
+				}
+			};
+			fileReader.readAsDataURL(file);
+		}
+		return () => {
+			isCancel = true;
+			if (fileReader && fileReader.readyState === 1) {
+				fileReader.abort();
+			}
+		};
+	}, [file]);
 	return (
 		<>
 			<div className="h-screen flex">
@@ -42,53 +82,56 @@ export default function lost_form() {
 							placeholder="Location"
 						/>
 					</div>
-					{/* <div
-						className="text-sm box-border flex items-center justify-center flex-wrap flex-col w-96 border-2 border-solid border-slate-400 rounded-md mb-5 bg-[#EBEFFA] pl-3 pr-3"
-						style={{ position: "relative" }}
-					>
-						<div>
-							<div className="text-center w-full text-base opacity-40">
-								Add Image
-							</div>
-							<div className=" text-center w-full scale-75 opacity-40">
-								<Image src={img1} className="" />
-							</div>
-
-							<div className="text-center w-full text-xs opacity-40 ">
-								Drag or drop or browse a file
-							</div>
-						</div>
+					{file === null ? (
 						<div
-							style={{
-								opacity: 0,
-								position: "absolute",
-							}}
+							className="text-sm box-border flex items-center justify-center flex-wrap flex-col w-96 border-2 border-solid border-slate-400 rounded-md mb-5 bg-[#EBEFFA] pl-3 pr-3"
+							style={{ position: "relative" }}
 						>
-							<FileUploader
-								handleChange={handleChange}
-								name="file"
-								types={fileTypes}
-							/>
-						</div>
-					</div> */}
+							<div>
+								<div className="text-center w-full text-base opacity-40">
+									Add Image
+								</div>
+								<div className=" text-center w-full scale-75 opacity-40">
+									<Image src={img1} className="" />
+								</div>
 
-					<div
-						className="text-sm box-border flex items-center justify-center flex-wrap flex-col w-96 border-2 border-solid border-slate-400 rounded-md mb-5 bg-[#EBEFFA] pl-3 pr-3"
-						style={{ position: "relative" }}
-					>
-						<div
-							className="py-4"
-							style={{ width: "50%", position: "relative" }}
-						>
-							<Image src={img2} />
-							<div style={{ position: "absolute", top: "10px", right: "10px" }}>
-								<IoCloseCircleOutline />
+								<div className="text-center w-full text-xs opacity-40 ">
+									Drag or drop or browse a file
+								</div>
+							</div>
+							<div
+								style={{
+									opacity: 0,
+									position: "absolute",
+								}}
+							>
+								<FileUploader
+									handleChange={handleChange}
+									name="file"
+									types={fileTypes}
+								/>
 							</div>
 						</div>
-					</div>
+					) : (
+						<div
+							className="text-sm box-border flex py-4 pt-6 items-center justify-center flex-wrap flex-col w-96 border-2 border-solid border-slate-400 rounded-md mb-5 bg-[#EBEFFA] pl-3 pr-3"
+							style={{ position: "relative" }}
+						>
+							<div className="" style={{ width: "50%", position: "relative" }}>
+								<img src={fileDataURL} alt="preview" />
+								<div
+									style={{ position: "absolute", top: "-15px", right: "-15px" }}
+									onClick={remove}
+								>
+									<IoCloseCircleOutline />
+								</div>
+							</div>
+						</div>
+					)}
 					<button
 						className="bg-[#304AC1] text-[#EBEFFA] h-11 w-96 rounded-md select-none"
 						type="button"
+						onClick={submit}
 					>
 						Add Lost Item
 					</button>
