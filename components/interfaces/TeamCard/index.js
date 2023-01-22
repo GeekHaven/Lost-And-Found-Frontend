c71faa9b29aac710fd23cd695f3d4b49f0346064
BuddyLongLegs <anurag10jain@gmail.com>
2023-01-22 11:03:15 +0530

    Updated Creators page

--- components/interfaces/TeamCard/index.js
+++ components/interfaces/TeamCard/index.js
@@ -1,15 +1,17 @@
-export default function TeamCard({img,name,role, link="#"}) {
-    return (
-        <div className="person">
-            <div className="container2">
-                <div className="container2-inner">
-                    <div className="circle"></div> 
-                    <img src={img} alt="" className="img img1" />         
-                </div>
-            </div>
-             <div className="divider"></div>
-            <a className="name2" href={link}>{name}</a>
-                    <div className="title2">{role}</div>
+export default function TeamCard({ img, name, role, link = "#", styles = "" }) {
+  return (
+    <div className="person">
+      <div className="container2">
+        <div className="container2-inner">
+          <div className="circle"></div>
+          <img src={img} alt="" className={"img " + styles} />
         </div>
-    )
+      </div>
+      <div className="divider"></div>
+      <a className="name2" href={link}>
+        {name}
+      </a>
+      <div className="title2">{role}</div>
+    </div>
+  );
 }
--- pages/team/index.js
+++ pages/team/index.js
@@ -3,7 +3,14 @@ import SEO from "../../components/utils/SEO";
 import Navbar2 from "../../components/interfaces/Navbar2";
 import Footer from "../../components/interfaces/Footer";
 import anurag from "../../assets/team/anurag.png";
+import manas from "../../assets/team/manas.png";
+import gadekar from "../../assets/team/gadekar.png";
+import yash from "../../assets/team/yash.png";
+import sibashish from "../../assets/team/sibashish.png";
+import aman from "../../assets/team/aman.png";
+import saishree from "../../assets/team/saishree.png";
 import nitya from "../../assets/team/nitya.png";
+
 export default function Team() {
   return (
     <>
@@ -17,7 +24,30 @@ export default function Team() {
             name={"BuddyLongLegs"}
             role={"Full Stack Developer"}
             link="https://buddylonglegs.live/"
+            styles="right-[50px]"
           />
+          <TeamCard
+            img={manas.src}
+            name={"Manas Gupta"}
+            role={"Front End Developer"}
+          />
+          <TeamCard
+            img={gadekar.src}
+            name={"Atharva Gadekar"}
+            role={"Front End Developer"}
+          />
+          <TeamCard
+            img={yash.src}
+            name={"Yash Gupta"}
+            role={"Front End Developer"}
+          />
+          <TeamCard
+            img={sibashish.src}
+            name={"Sibashish Behra"}
+            role={"Backend Developer"}
+          />
+          <TeamCard img={aman.src} name={"Aman Karnawat"} role={"Designer"} />
+          <TeamCard img={saishree.src} name={"Saishree"} role={"Designer"} />
           <TeamCard img={nitya.src} name={"Nitya Gupta"} role={"Designer"} />
         </div>
       </div>
