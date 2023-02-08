f201378e11992c0b31ecd8795170c86f6e385ec1
BuddyLongLegs <anurag10jain@gmail.com>
2023-02-09 01:21:03 +0530

    Added prod configs

--- components/utils/API/index.js
+++ components/utils/API/index.js
@@ -1,94 +1,91 @@
 import axios from "axios";
 import { getLS, removeLS } from "../LocalStorage/index";
 
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.API_URL;
 
 const getAccessToken = () => {
-    return getLS("jwt_token");
+  return getLS("jwt_token");
 };
 
 const getHeaders = (token) => {
-    if (!token) token = getAccessToken();
-    if (token) {
-        return {
-            headers: {
-                Accept: "application/json",
-                Authorization: token,
-            },
-        };
-    }
+  if (!token) token = getAccessToken();
+  if (token) {
     return {
-        headers: {
-            Accept: "application/json",
-        },
+      headers: {
+        Accept: "application/json",
+        Authorization: token,
+      },
     };
+  }
+  return {
+    headers: {
+      Accept: "application/json",
+    },
+  };
 };
 
 const post = async (endpoint, body, token = null, form = false) => {
-    let options = getHeaders(token);
-    if (form) {
-        options.headers["Content-Type"] = "multipart/form-data";
+  let options = getHeaders(token);
+  if (form) {
+    options.headers["Content-Type"] = "multipart/form-data";
+  }
+  try {
+    const response = await axios.post(API_URL + endpoint, body, options);
+    return response;
+  } catch (err) {
+    console.error(err?.response?.data || err);
+    if (err?.response?.status === 401) {
+      console.log("Wrong password");
+      removeLS("jwt_token");
     }
-    try {
-      const response = await axios.post(API_URL + endpoint, body, options);
-      return response;
-    } catch (err) {
-      console.error(err?.response?.data || err);
-      if (err?.response?.status === 401) {
-        console.log("Wrong password");
-        removeLS("jwt_token");
-      }
-      return err?.response?.data || err;
-    }
+    return err?.response?.data || err;
+  }
 };
 
 const get = async (endpoint, token = null) => {
-    try {
-        const response = await axios.get(API_URL + endpoint, getHeaders(token));
-        return response;
-    } catch (err) {
-        console.error(err?.response?.data || err);
-        if (err?.response?.status === 401) {
-            console.log("Wrong password");
-            removeLS("jwt_token");
-        }
-        return err?.response?.data || err;
+  try {
+    const response = await axios.get(API_URL + endpoint, getHeaders(token));
+    return response;
+  } catch (err) {
+    console.error(err?.response?.data || err);
+    if (err?.response?.status === 401) {
+      console.log("Wrong password");
+      removeLS("jwt_token");
     }
-};  
+    return err?.response?.data || err;
+  }
+};
 
 const put = async (endpoint, body, token = null) => {
-    try {
-        const response = await axios.put(
-            API_URL + endpoint,
-            body,
-            getHeaders(token)
-        );
-        return response.data;
-    } catch (err) {
-        console.error(err?.response?.data || err);
-        if (err?.response?.status === 401) {
-            console.log("Wrong password");
-            removeLS("jwt_token");
-        }
-        return err?.response?.data || err;
+  try {
+    const response = await axios.put(
+      API_URL + endpoint,
+      body,
+      getHeaders(token)
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err?.response?.data || err);
+    if (err?.response?.status === 401) {
+      console.log("Wrong password");
+      removeLS("jwt_token");
     }
+    return err?.response?.data || err;
+  }
 };
 
 const remove = async (endpoint, token = null) => {
-    try {
-        const response = await axios.delete(
-            API_URL + endpoint,
-            getHeaders(token)
-        );
-        return response.data;
-    } catch (err) {
-        console.error(err?.response?.data || err);
-        if (err?.response?.status === 401) {
-            console.log("Wrong password");
-            removeLS("jwt_token");
-        }
-        return err?.response?.data || err;
+  try {
+    const response = await axios.delete(API_URL + endpoint, getHeaders(token));
+    return response.data;
+  } catch (err) {
+    console.error(err?.response?.data || err);
+    if (err?.response?.status === 401) {
+      console.log("Wrong password");
+      removeLS("jwt_token");
     }
+    return err?.response?.data || err;
+  }
 };
 
 export { getAccessToken, post, get, put, remove, API_URL, getHeaders };
--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
+let dotenv = require("dotenv");
+dotenv.config();
+
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-		domains: [
-			"smf-file-storage.s3.ap-south-1.amazonaws.com",
-			"localhost",
-			"172.19.12.25",
-		],
-	},
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains:
+      process.env.ENV === "PROD"
+        ? ["lnf.iiita.ac.in"]
+        : [
+            "smf-file-storage.s3.ap-south-1.amazonaws.com",
+            "localhost",
+            "172.19.12.25",
+            "172.19.14.254",
+          ],
+    unoptimized: true,
+  },
+  env: {
+    API_URL: process.env["API"],
+  },
 };
 
 module.exports = nextConfig;
