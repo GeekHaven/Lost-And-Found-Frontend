9d9aff1114811cd39dc992f7f094ecf1fcad66b2
BuddyLongLegs <anurag10jain@gmail.com>
2023-02-10 01:53:47 +0530

    Changed Category card

--- components/interfaces/CategoryCard/index.js
+++ components/interfaces/CategoryCard/index.js
@@ -1,41 +1,38 @@
 import Image from "next/image";
 import CategoryCardPlaceholder from "../../../assets/logo/CategoryCardPlaceholder.svg";
 export default function CategoryCard({ data, setSelectedCategory }) {
-    //set the selected category card
-    function handleClick() {
-        setSelectedCategory(data.id);
-    }
+  //set the selected category card
+  function handleClick() {
+    setSelectedCategory(data.id);
+  }
 
-    return (
+  return (
+    <div
+      className="h-[252px] w-[200px] sm:h-[80px] sm:w-[80px] flex flex-col items-center bg-[#e69d96] cursor-pointer "
+      style={{
+        position: "relative",
+        boxShadow: "0px 2px 8px #babfd8",
+        // width: "200px",
+        // height: "200px",
+      }}
+      onClick={handleClick}
+    >
+      <Image
+        src={data.img || CategoryCardPlaceholder}
+        width="200px"
+        height="200px"
+      />
+      <div className="sm:hidden bg-[#f2f2f2]">
         <div
-            className="h-[200px] w-[200px] sm:h-[80px] sm:w-[80px] flex flex-col items-center bg-[#e69d96] cursor-pointer "
-            style={{
-                position: "relative",
-                boxShadow: "0px 2px 8px #babfd8",
-                // width: "200px",
-                // height: "200px",
-            }}
-            onClick={handleClick}
+          className="text-center py-5 text-xl font-medium text-[#2f3551] "
+          style={{
+            width: "200px",
+            height: "52px",
+          }}
         >
-            <div>
-                <Image
-                    src={data.img || CategoryCardPlaceholder}
-                    className="w-52 h-36"
-                    width="200px"
-                    height="148px"
-                />
-            </div>
-            <div className="sm:hidden bg-[#f2f2f2]">
-                <div
-                    className="text-center py-3 text-xl font-medium text-[#2f3551] "
-                    style={{
-                        width: "200px",
-                        height: "52px",
-                    }}
-                >
-                    {data.name}
-                </div>
-            </div>
+          {data.name}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- components/interfaces/CategoryCardSelected/index.js
+++ components/interfaces/CategoryCardSelected/index.js
@@ -3,53 +3,51 @@ import { BsCheckCircleFill } from "react-icons/bs";
 import CategoryCardPlaceholder from "../../../assets/logo/CategoryCardPlaceholder.svg";
 
 export default function CategoryCardSelected({ data, setSelectedCategory }) {
-    //unset the selected category card
-    function handleClick() {
-        setSelectedCategory("");
-    }
+  //unset the selected category card
+  function handleClick() {
+    setSelectedCategory("");
+  }
 
-    return (
+  return (
+    <div
+      className="h-[252px] w-[200px] sm:h-[80px] sm:w-[80px] flex flex-col items-center bg-[#e69d96] cursor-pointer border-4 border-[#304AC1] relative"
+      style={{
+        boxShadow: "0px 2px 8px #babfd8",
+        // width: "200px",
+        // height: "200px",
+      }}
+      onClick={handleClick}
+    >
+      <Image
+        src={data.img || CategoryCardPlaceholder}
+        width="200px"
+        height="200px"
+      />
+      <div className="sm:hidden bg-[#f2f2f2]">
         <div
-            className="h-[200px] w-[200px] sm:h-[80px] sm:w-[80px] flex flex-col items-center bg-[#e69d96] cursor-pointer border-4 border-[#304AC1] relative"
-            style={{
-                boxShadow: "0px 2px 8px #babfd8",
-                // width: "200px",
-                // height: "200px",
-            }}
-            onClick={handleClick}
+          className="text-center border-x-4 border-[#304AC1] border-t-0 py-5 text-xl font-medium text-[#2f3551] "
+          style={{
+            width: "200px",
+            height: "52px",
+          }}
         >
-            <div>
-                <Image
-                    src={data.img || CategoryCardPlaceholder}
-                    width="200px"
-                    height="148px"
-                />
-            </div>
-            <div className="sm:hidden bg-[#f2f2f2]">
-                <div
-                    className="text-center border-4 border-[#304AC1] border-t-0 py-3 text-xl font-medium text-[#2f3551] "
-                    style={{
-                        width: "200px",
-                        height: "52px",
-                    }}
-                >
-                    {data.name}
-                </div>
-                <div>
-                    <BsCheckCircleFill
-                        color="#304AC1"
-                        size={30}
-                        style={{
-                            position: "absolute",
-                            backgroundColor: "white",
-                            borderRadius: "50%",
-                            top: 0,
-                            right: 0,
-                            transform: "translate(50%, -50%)",
-                        }}
-                    />
-                </div>
-            </div>
+          {data.name}
         </div>
-    );
+        <div>
+          <BsCheckCircleFill
+            color="#304AC1"
+            size={30}
+            style={{
+              position: "absolute",
+              backgroundColor: "white",
+              borderRadius: "50%",
+              top: 0,
+              right: 0,
+              transform: "translate(50%, -50%)",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
